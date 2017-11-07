ca219eb8049b2f46b1003193e110511600911585
Johnny Reina <johnny.r.reina@gmail.com>
2017-11-07 01:28:09 -0600

    Adds c# 7.1 features. Moves c# 7 stuff to its own file. Updates language to c# 7.1.

---
+++ c-sharp-7-1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace new_csharp_features
+{
+    class async_main_method
+    {
+        static async Task Main(string[] args)
+        {
+            await Task.Run(() => WriteLine("Hello World!"));
+            return;
+        }
+    }
+
+    class default_literal_expressions
+    {
+        Dictionary<string, string> emptyCtr = new Dictionary<string, string>();
+        Dictionary<string, string> withDefault = default(Dictionary<string, string>);
+        Dictionary<string, string> withDefaultLiteral = default;
+    }
+
+    class more_tuple_goodies
+    {
+        void inferred_tuple_element_names()
+        {
+            var count = 5;
+            var name = "Salad";
+            var pair = (count, name);
+
+            WriteLine($"count: {pair.count}, name: {pair.name}");
+        }
+    }
+}
